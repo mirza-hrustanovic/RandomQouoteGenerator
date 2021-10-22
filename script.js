62070b62bbc7918bbfa8d54684645e6f5092521e
Mirza Hrustanović <mirzahrustanovic1@gmail.com>
2021-10-22 11:26:04 +0200

    added index.html style.css script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+const quotes = [
+  {
+    name: "Franklin D. Roosevelt",
+    quote:
+      "When you reach the end of your rope, tie a knot in it and hang on. ",
+  },
+  {
+    name: "Robert Louis Stevenson",
+    quote:
+      "Don't judge each day by the harvest you reap but by the seeds that you plant. ",
+  },
+  {
+    name: "Benjamin Franklin",
+    quote:
+      "Tell me and I forget. Teach me and I remember. Involve me and I learn. ",
+  },
+  {
+    name: " Aristotle",
+    quote:
+      "It is during our darkest moments that we must focus to see the light. ",
+  },
+  {
+    name: " Nelson Mandela",
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall. ",
+  },
+  {
+    name: " Steve Jobs",
+    quote:
+      "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. ",
+  },
+  {
+    name: " Walt Disney",
+    quote: "The way to get started is to quit talking and begin doing. ",
+  },
+  {
+    name: " James Cameron",
+    quote:
+      "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. ",
+  },
+  {
+    name: " John Lennon",
+    quote: "Life is what happens when you're busy making other plans. ",
+  },
+  {
+    name: " Ralph Waldo Emerson",
+    quote:
+      "Do not go where the path may lead, go instead where there is no path and leave a trail. ",
+  },
+];
